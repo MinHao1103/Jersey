6ba4669614dfaba0d24db9c95382cdd8771f7252
Ace <ooxxqaws@gmail.com>
2024-04-22 00:58:07 +0800

    <1.0.1> [modify] 調整 AMultiplyB 與 ADivideB 的 Log 訊息

--- src/main/java/com/example/service/http/servlet/Calculator.java
+++ src/main/java/com/example/service/http/servlet/Calculator.java
@@ -4,7 +4,6 @@ import com.example.service.http.utils.httpResult.CommonHttpResult;
 import com.example.service.http.utils.log.Log;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 
 @Path("/v1")
 public class Calculator {
@@ -12,22 +11,26 @@ public class Calculator {
     private static final String TAG = "Calculator";
 
     @GET
-    @Path("/APlusB")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public CommonHttpResult<Integer> aPlusB(@QueryParam("a") int a, @QueryParam("b") int b) {
-        Log.d(TAG, String.format("Parameter a: %s, Parameter b: %s", a, b));
-        int result = a + b;
-        return new CommonHttpResult<Integer>(0, result);
+    @Path("/AMultiplyB")
+    @Produces({"application/json"})
+    @Consumes({"application/json"})
+    public CommonHttpResult<Integer> aMultiplyB(@QueryParam("a") int a, @QueryParam("b") int b) {
+        int result = a * b;
+        Log.d(TAG, String.format("a: %s, b: %s, result: %s * %s = %s", a, b, a, b, result));
+        return new CommonHttpResult(0, result);
     }
 
     @GET
-    @Path("/AMinusB")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public CommonHttpResult<Integer> aMinusB(@QueryParam("a") int a, @QueryParam("b") int b) {
-        Log.d(TAG, String.format("Parameter a: %s, Parameter b: %s", a, b));
-        int result = a - b;
-        return new CommonHttpResult<Integer>(0, result);
+    @Path("/ADivideB")
+    @Produces({"application/json"})
+    @Consumes({"application/json"})
+    public CommonHttpResult<Float> aDivideB(@QueryParam("a") float a, @QueryParam("b") float b) {
+        if (b == 0) {
+            return new CommonHttpResult(1, "Cannot divide by zero");
+        }
+        float result = a / b;
+        Log.d(TAG, String.format("a: %s, b: %s, result: %s / %s = %s", a, b, a, b, result));
+        return new CommonHttpResult(0, result);
     }
+
 }
